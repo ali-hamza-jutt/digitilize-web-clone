4e73e40c60cea8e878003f5fd66e12fefee8f341
ali-hamza-jutt <alihamza16jutt@outlook.com>
2024-09-10 16:28:17 +0500

    Navbar Design changes
    -- Made Navbar position sticky
    -- For Navbar links About, Services, Packages made dropdown menu that create further options upon hovering that links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes, faPhone,faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 const Navbar = () => {
+  const aboutUsOptions = ['Why Us', 'Testimonials', 'Awards & Recognition', 'Partners & Clients', 'Our Values'];
+  const packagesOptions = ['Social Media Marketing', 'Email Marketing', 'Optimization Packages', 'SEO Pricing', 'Brand Identity'];
+  const servicesOptions = ['Web Development', 'Mobile App Development', 'UI/UX Design', 'Web Hosting'];
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -23,10 +27,33 @@ const Navbar = () => {
           <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
         </div>
         <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Case Studies</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Packages</a></li>
+          <li className="dropdown">
+            <a href="#">Services <FontAwesomeIcon icon={faAngleDown} /></a>
+            <ul className="dropdown-menu">
+              {servicesOptions.map((option, index) => (
+                <li key={index}><a href="#">{option}</a></li>
+              ))}
+            </ul>
+          </li>
+          <li className="dropdown">
+            <a href="#">Case Studies</a>
+          </li>
+          <li className="dropdown">
+            <a href="#">About Us <FontAwesomeIcon icon={faAngleDown}/></a>
+            <ul className="dropdown-menu">
+              {aboutUsOptions.map((option, index) => (
+                <li key={index}><a href="#">{option}</a></li>
+              ))}
+            </ul>
+          </li>
+          <li className="dropdown">
+            <a href="#">Packages <FontAwesomeIcon icon={faAngleDown}/></a>
+            <ul className="dropdown-menu">
+              {packagesOptions.map((option, index) => (
+                <li key={index}><a href="#">{option}</a></li>
+              ))}
+            </ul>
+          </li>
           <li><a href="#">Contact Us</a></li>
           <li className="contact-info">
             <a href="#"><FontAwesomeIcon icon={faPhone} className="fa-icon" /> +44 7947 430 685</a>
